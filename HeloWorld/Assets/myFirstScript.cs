2be718abb5d21893f1ab53d38f7fc3673fb5bcbe
KennyBeverley <kenneth.beverley@gmail.com>
2020-09-07 11:26:40 -0500

    Created HelloWorld

---
+++ HeloWorld/Assets/myFirstScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myFirstScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Hello World Awake");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello World Update");
+    }
+}
